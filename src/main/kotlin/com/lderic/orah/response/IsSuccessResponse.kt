d112282187ac4842479294e860c4af821dc8503a
Eric <lderic0415@gmail.com>
2022-05-11 00:42:50 -0700

    Delete unnecessary files

--- src/main/kotlin/com/lderic/orah/response/IsSuccessResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.lderic.orah.response
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class IsSuccessResponse(
-    val success: Boolean
-)
--- src/main/kotlin/com/lderic/orah/util/Ids.kt
+++
@@ -1,8 +0,0 @@
-package com.lderic.orah.util
-
-// Locations
-object Ids {
-    const val JUSTIN_SIENA = 7689
-    const val SARATOGA_DOWNS = 7212
-    const val MAHER_DORM = 7213
-}
